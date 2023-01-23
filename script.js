2a5fcdb2305dcfcd5451472ef27049089bac39a3
henrypeters <meethenry09@gmail.com>
2023-01-23 19:59:38 +0100

    feat: add square and divide

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
 const input = document.getElementById("input");
-const addedNumber = document.getElementById("added-num");
+const dividedNumber = document.getElementById("divide");
 const evenOrOdd = document.getElementById("even-odd");
+const clearButton = document.getElementById("clear-button");
+const squaredNumber = document.getElementById("square")
 
 let even = "even";
 let odd = "odd";
 
-function getAddedNumber(value) {
-    if (value > 0) {
-        return Number.parseInt(value, 10) + 1;
-    } else {
+function getDividedNumber(value) {
+    if (input.value > 0) {
+        return value/2;
+    }else{
         return 0;
     }
 }
 
 function numberVerification(input) {
     if (input % 2) {
-        return even;
-    }else {
         return odd;
+    }else {
+        return even;
     }
 }
 
-function getnumber() {
-    addedNumber.textContent = getAddedNumber(input.value);
-    getEvenOrOdd();
-    return
+function squaredAnwer(x) {
+    if (input.value > 0) {
+        return x**2;
+    }else {
+        return 0;
+    }
 }
 
 function getEvenOrOdd() {
     evenOrOdd.textContent = numberVerification(Number.parseInt(input.value, 10))
 }
 
+function getSquare() {
+    squaredNumber.textContent = squaredAnwer(Number.parseInt(input.value, 10))
+}
 
-input.addEventListener("keyup", getnumber)
+function getAnswer() {
+    dividedNumber.textContent = getDividedNumber(Number.parseInt(input.value));
+    getEvenOrOdd();
+    getSquare();
+    return
+}
+
+clearButton.addEventListener("click", function() {
+    input.value = "";
+})
+
+input.addEventListener("keyup", getAnswer)
